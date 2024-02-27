999d08a45dabc7c07a3619dd8692cb127c3ef6d7
Fuma Nama <god63820869@gmail.com>
2024-02-27 21:40:40 +0800

    Update playground code

--- app/(full)/webgl/renderer.ts
+++ app/(full)/webgl/renderer.ts
@@ -8,160 +8,92 @@ type ExtendedInstanceProps = InstanceProps & {
 };
 type Instance = ReturnType<Phenomenon["add"]>;
 
-const multiplier = 40000;
-const begin = 0.4;
-const duration = 0.6;
-
-function h2r(p: number, q: number, t: number) {
-  if (t < 0) t += 1;
-  if (t > 1) t -= 1;
-  if (t < 1 / 6) return p + (q - p) * 6 * t;
-  if (t < 1 / 2) return q;
-  if (t < 2 / 3) return p + (q - p) * 6 * (2 / 3 - t);
-  return p;
-}
-
-function getHSL(h: number, s: number, l: number) {
-  h = ((h % 1) + 1) % 1;
-  s = Math.max(0, Math.min(1, s));
-  l = Math.max(0, Math.min(1, l));
-  if (s === 0) return [l, l, l];
-  const p = l <= 0.5 ? l * (1 + s) : l + s - l * s;
-  const q = 2 * l - p;
-  return [h2r(q, p, h + 1 / 3), h2r(q, p, h), h2r(q, p, h - 1 / 3)];
-}
-
-function getRandom(value: number) {
-  const floor = -value;
-  return floor + Math.random() * value * 2;
-}
-
 const vertex = `
-  attribute vec3 aPositionStart;
-  attribute vec3 aControlPointOne;  
-  attribute vec3 aControlPointTwo;  
-  attribute vec3 aPositionEnd;  
-  attribute vec3 aPosition;  
-  attribute vec3 aColor;  
-  attribute float aOffset;  
-
-  uniform float uProgress;
-  uniform mat4 uProjectionMatrix;
-  uniform mat4 uModelMatrix;
-  uniform mat4 uViewMatrix;
-
-  varying vec3 vColor;
-
-  vec3 bezier4(vec3 a, vec3 b, vec3 c, vec3 d, float t) {
-    return mix(mix(mix(a, b, t), mix(b, c, t), t), mix(mix(b, c, t), mix(c, d, t), t), t);
-  }
+  precision mediump float;
+  attribute vec3 aPosition;
 
   void main(){
-    float tProgress = min(1.0, max(0.0, (uProgress - aOffset)) / ${duration});
-    vec3 newPosition = bezier4(aPositionStart, aControlPointOne, aControlPointTwo, aPositionEnd, tProgress);
-    gl_PointSize = 4.0;
-    gl_Position = uProjectionMatrix * uModelMatrix * uViewMatrix * vec4(newPosition + aPosition, 1.0);
-    vColor = aColor;
+    gl_Position = vec4(aPosition, 1.);
   }
 `;
 
+// Ether by nimitz 2014 (twitter: @stormoid)
+// https://www.shadertoy.com/view/MsjSW3
+// License Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License
+
 const fragment = `
-  precision mediump float;
+precision mediump float;
+uniform float u_time;
+uniform vec2 u_resolution;
 
-  varying vec3 vColor;
+mat2 m(float a){
+  float c=cos(a);
+  float s=sin(a);
+  return mat2(c,-s,s,c);
+}
 
-  void main(){
-    gl_FragColor = vec4(vColor, 1.0);
-  }
+float map(vec3 p){
+    p.xz*= m(u_time*0.4);p.xy*= m(u_time*0.3);
+    vec3 q = p*2.+u_time;
+    return length(p+vec3(sin(u_time*0.7)))*log(length(p)+1.) + sin(q.x+sin(q.z+sin(q.y)))*0.5 - 1.;
+}
+
+void main(){	
+	vec2 p = gl_FragCoord.xy/u_resolution.y - vec2((u_resolution.x/2.0 - 2.5)/u_resolution.y,.5);
+  vec3 cl = vec3(0.);
+  float d = 2.5;
+  for(int i=0; i<=5; i++)	{
+		vec3 p = vec3(0,0,5.) + normalize(vec3(p, -1.))*d;
+        float rz = map(p);
+		float f =  clamp((rz - map(p+.1))*0.5, -.1, 1. );
+        vec3 l = vec3(0.1,0.3,.4) + vec3(5., 2.5, 3.)*f;
+        cl = cl*l + smoothstep(2.5, .0, rz)*.7*l;
+		d += min(rz, 1.);
+	}
+
+  gl_FragColor = vec4(cl, 1.);
+}
 `;
 
 export function createRenderer(canvas: HTMLCanvasElement): Phenomenon {
   const renderer = new Phenomenon({
     canvas,
     settings: {
-      clearColor: [5 / 255, 100 / 255, 100 / 255, 1],
-      position: { x: 0, y: 0, z: 1.2 },
+      alpha: true,
+      position: { x: 0, y: 0, z: 1 },
       shouldRender: true,
     },
   });
 
-  const attributes: InstanceProps["attributes"] = [
-    {
-      name: "aPositionStart",
-      data: () => {
-        return [0, -1, 0];
-      },
-      size: 3,
-    },
-    {
-      name: "aControlPointOne",
-      data: (index: number, total: number) => {
-        const angle = index * ((2 * Math.PI) / total);
-        return [
-          Math.cos(angle) * 1.5 + getRandom(0.5),
-          1.5 + getRandom(0.5),
-          Math.sin(angle) * 1.5,
-        ];
-      },
-      size: 3,
-    },
-    {
-      name: "aControlPointTwo",
-      data: (index: number, total: number) => {
-        const angle = index * ((2 * Math.PI) / total);
-        return [
-          Math.cos(angle) + getRandom(1),
-          1 + getRandom(0.5),
-          Math.sin(angle) + getRandom(1),
-        ];
-      },
-      size: 3,
-    },
-    {
-      name: "aPositionEnd",
-      data: (index: number, total: number) => {
-        const angle = index * ((2 * Math.PI) / total);
-        return [Math.cos(angle) + getRandom(2), -2, 0];
-      },
-      size: 3,
-    },
-    {
-      name: "aColor",
-      data: (index: number, total: number) =>
-        getHSL(
-          begin + (index / total) * 0.2,
-          0.6 + getRandom(0.1),
-          0.6 + getRandom(0.1)
-        ),
-      size: 3,
-    },
-    {
-      name: "aOffset",
-      data: (i: number) => [i * ((1 - duration) / (multiplier - 1))],
-      size: 1,
-    },
-  ];
-
-  const uniforms: InstanceProps["uniforms"] = {
-    uProgress: {
-      type: "float",
-      value: [0.0],
-    },
-  };
-
   renderer.add("starling", {
-    attributes,
-    multiplier,
-    uniforms,
+    mode: 4,
     vertex,
+    geometry: {
+      vertices: [
+        { x: -100, y: 100, z: 0 },
+        { x: -100, y: -100, z: 0 },
+        { x: 100, y: 100, z: 0 },
+        { x: 100, y: -100, z: 0 },
+        { x: -100, y: -100, z: 0 },
+        { x: 100, y: 100, z: 0 },
+      ] as unknown as object[][],
+    },
     fragment,
-    onRender(instance) {
-      const uProgress = instance.uniforms.uProgress;
-
-      uProgress.value[0] += 0.004;
-      if (uProgress.value[0] >= 1.2) {
-        uProgress.value[0] = 0;
-      }
+    uniforms: {
+      u_resolution: {
+        type: "vec2",
+        value: [
+          canvas.width * window.devicePixelRatio,
+          canvas.height * window.devicePixelRatio,
+        ],
+      },
+      u_time: {
+        type: "float",
+        value: [0.0],
+      },
+    },
+    onRender: ({ uniforms }) => {
+      uniforms.u_time.value[0] += 0.01;
     },
   } as ExtendedInstanceProps);
 
